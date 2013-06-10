8d1daf4b2a8e39d45f6fc4e8bc99084bfda85eff
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-10 19:58:21 +0300

    bp main nav is now a navbar

--- members/single/home.php
+++ members/single/home.php
@@ -18,11 +18,12 @@ get_header( 'buddypress' ); ?>
 			</div><!-- #item-header -->
 
 			<div id="item-nav">
-				<div class="item-list-tabs no-ajax btn-group" id="object-nav" role="navigation">
-
-						<?php shoestrap_bp_get_displayed_user_nav(); ?>
+				<div class="item-list-tabs no-ajax navbar" id="object-nav" role="navigation">
+          <ul class="nav navbar-nav">
+						<?php bp_get_displayed_user_nav(); ?>
 
 						<?php do_action( 'bp_member_options_nav' ); ?>
+          </ul>
 
 				</div>
 			</div><!-- #item-nav -->
